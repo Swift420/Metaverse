775b86c7ec9e1f8f45dc2c747cfaf1f7db013fd6
Swift420 <apollosdavid95@gmail.com>
2022-12-16 12:40:44 +0200

    deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 };
 
 module.exports = nextConfig;
